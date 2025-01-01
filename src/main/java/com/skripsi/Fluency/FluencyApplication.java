3017e06d93abcfb72adeee55347e2f59c8e570fd
devindevin278 <devindevin278@gmail.com>
2025-01-01 22:30:19 +0700

    initial commit

---
+++ src/main/java/com/skripsi/Fluency/FluencyApplication.java
@@ -0,0 +1,13 @@
+package com.skripsi.Fluency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluencyApplication.class, args);
+	}
+
+}
