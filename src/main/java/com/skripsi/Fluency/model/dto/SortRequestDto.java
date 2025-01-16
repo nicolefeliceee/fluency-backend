6f28865ac1ae2969fd381aab613b3122b83092c1
nicolefeliceee <nicolefeliceee@gmail.com>
2025-01-17 01:39:39 +0700

    sort influencer done

---
+++ src/main/java/com/skripsi/Fluency/model/dto/SortRequestDto.java
@@ -0,0 +1,26 @@
+package com.skripsi.Fluency.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SortRequestDto {
+    private List<String> followers2;
+    private List<Integer> media2;
+    //    private List<String> engagement;
+    private List<Integer> gender2;
+    private List<String> age2;
+    private List<String> price2;
+    private List<String> rating2;
+    private List<Integer> location2;
+    private List<Integer> genderAudience2;
+    private List<String> ageAudience2;
+    private List<Integer> sort;
+}
